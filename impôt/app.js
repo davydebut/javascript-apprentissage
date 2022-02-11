962a15185150b39ee4fe3fb993d14367c635fff9
davydebut <spanudavy@gmail.com>
2022-02-11 10:50:17 +0100

    impots terminé

--- impôt/app.js
+++ impôt/app.js
@@ -1,3 +1,26 @@
 // si homme > 20 ans alors paie l'impôt
 // si femme > 18 && < 35 alors paie l'impôt
 // sinon les autres ne paie pas l'impôt
+
+let ageSaisi,
+    genreSaisi,
+    bouton = document.getElementById('bouton'),
+    message = document.getElementById('message');
+
+
+function verifierImposition() {
+    // récupérer la valeur de l'age et du genre avec la fonction .value
+    ageSaisi = document.getElementById('age-saisi').value;
+    genreSaisi = document.getElementById('genre-saisi').value;
+    // Notre condition : Si âge >= 18 et Homme alors, ou si femme et âge >= 20 et si femme...
+    if(genreSaisi == "H" && ageSaisi >= 18){
+        message.innerHTML = '<div class="alert alert-danger" role="alert">Vous êtes imposable</div>';
+    }else if((genreSaisi == "F" && ageSaisi >= 20) && (genreSaisi == "F" && ageSaisi <= 35)){
+        message.innerHTML = '<div class="alert alert-danger" role="alert">Vous êtes imposable</div>';
+    }else{
+        message.innerHTML = '<div class="alert alert-success" role="alert">Vous êtes non imposable !</div>';
+    }
+}
+
+// Ajout d'un écouteur d'événement sur le bouton qui au click appelera la fonction verifierImposition();
+bouton.addEventListener('click', verifierImposition, false);
