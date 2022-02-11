cfdc1d1ede531d551b15fe981e561d45bb4a854d
davydebut <spanudavy@gmail.com>
2022-02-11 13:44:23 +0100

    magasin facture javascript terminé

--- magasin/app.js
+++ magasin/app.js
@@ -1,2 +1,20 @@
-let n, p;
+let nbrePhotocopie,
+    prixTotal,
+    bouton = document.getElementById('bouton'),
+    message = document.getElementById('message');
 
+function effectuerCalcul(){
+    nbrePhotocopie = document.getElementById('valeur-saisie').value;
+
+    if(nbrePhotocopie <= 10){
+        prixTotal = nbrePhotocopie * 0.1;
+    }else if(nbrePhotocopie <= 30){
+        prixTotal = nbrePhotocopie * 0.1 + (nbrePhotocopie - 10) * 0.09;
+    }else{
+        prixTotal = nbrePhotocopie * 0.1 + 20 * 0.09 + (nbrePhotocopie - 30) * 0.08;
+    }
+
+    message.innerHTML = "Le prix total est de : " + prixTotal + " euros";
+}
+
+bouton.addEventListener('click', effectuerCalcul, false);
