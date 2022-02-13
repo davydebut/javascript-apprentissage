f9d671a71c3a39dc63eb17a4b2e06eba9febe11f
davydebut <spanudavy@gmail.com>
2022-02-13 14:17:05 +0100

    Exercice rendu de monnaie corrigé
    impossible d'afficher la valeur de rendu

--- Rendu_de_monnaie/app.js
+++ Rendu_de_monnaie/app.js
@@ -1,5 +1,40 @@
-let totalSaisi = document.getElementById('total'),
-    monnaieSaisi = document.getElementById('monnaie'),
+let totalSaisi = document.getElementById('total').value,
+    monnaieSaisi = document.getElementById('monnaie').value,
+    message10 = document.getElementById('message10'),
+    message5 = document.getElementById('message5'),
+    message1 = document.getElementById('message1'),
+    bouton = document.getElementById('button'),
     aRendre;
 
-// aRendre = totalSaisi - monnaieSaisi
+// aRendre = monnaieSaisi - totalSaisi
+// créer une boucle qui incrémente combien de 10 il y a dans aRendre
+// while(aRendre >= 10){
+//     billet10++;
+//     aRendre = aRendre - 10;
+// }
+// et on fait pareil pour le billet de 5 et la pièce de 1
+// Enfin on affiche le résultat du nombre de billet et de pièces
+
+function operation(){
+    aRendre = monnaieSaisi - totalSaisi;
+    // aRendre = 150 - 125;
+    billet10 = 0;
+    billet5 = 0;
+    while(aRendre >= 10){
+        billet10++;
+        aRendre = aRendre - 10;
+    }
+    while(aRendre >= 5){
+        billet5++;
+        aRendre = aRendre - 5;
+    }
+    message10.innerHTML = billet10;
+    message5.innerHTML = billet5;
+    message1.innerHTML = aRendre;
+
+    // console.log(billet10);
+    // console.log(billet5);
+    // console.log(aRendre);
+}
+
+bouton.addEventListener('click', operation, false);
