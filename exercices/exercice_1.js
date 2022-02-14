95df495da1a9c772a48bbed1179ee10695c1a902
davydebut <spanudavy@gmail.com>
2022-02-14 15:40:05 +0100

    Exercice d'algo du formateur

--- exercices/exercice_1.js
+++ exercices/exercice_1.js
@@ -1,6 +1,15 @@
+/* exos 1
+
 // Aire se calcul par longeur multiplié par largeur
 // un carré de 4cm par 4cm
 
 let longeur = 4, largeur = 4, aire = longeur*largeur;
 
 console.log(aire);
+
+fin exos 1 */
+
+/* exos 2 
+Carré d'un chiffre = 
+*/
+
