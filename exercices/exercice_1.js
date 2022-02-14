ebbca23a4e30e7ec25bd00f88ef92d42f20f2c00
davydebut <spanudavy@gmail.com>
2022-02-14 16:11:16 +0100

    Fin de l'exercice 3 du 1er pdf algo

--- exercices/exercice_1.js
+++ exercices/exercice_1.js
@@ -10,6 +10,40 @@ console.log(aire);
 fin exos 1 */
 
 /* exos 2 
-Carré d'un chiffre = 
-*/
+Carré d'un chiffre = chiffre * chiffre;
+
+
+let chiffre = 16;
+
+let
+    // chiffre = prompt("Entrer valeur", "0"),
+    num = parseInt(chiffre),
+    carre = num * num;
+
+console.log("carré de " + num + " est : " + carre);
+
+fin exos 2 */
+
+/* exos 3
+
+Année actuelle : let Annee = new Date().getFullYear()
+Ensuite tu calcule l'année actuelle moins l'âge de l'utilisateur,
+let nbre = prompt("Entrez votre âge", "0");
+let age = parseInt(nbre);
+let naissance = Annee - age;
+Votre année de naissance est : naissance
+
+let Annee = new Date().getFullYear(),
+    nbre = 16,
+    // nbre = prompt("Entrez votre âge", "0"),
+    age = parseInt(nbre),
+    naissance = Annee - age;
+
+    console.log("Votre année de naissance est : " + naissance)
+
+fin exos 3 */
+
+
+
+
 
