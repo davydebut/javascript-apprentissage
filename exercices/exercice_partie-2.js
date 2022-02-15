4251221fc2d790793f8cf50169ffb774b7827570
davydebut <spanudavy@gmail.com>
2022-02-15 17:05:19 +0100

    Exercices de la partie 2 des Algos
    MAJ : Exos 5

--- exercices/exercice_partie-2.js
+++ exercices/exercice_partie-2.js
@@ -1,6 +1,7 @@
-// Exo 1
+/* // Exo 1
 
-let note = prompt('Quel est votre note ?');
+// let note = prompt('Quel est votre note ?');
+let note = 10;
 
 if (note >= 12) {
     console.log('Tu as la moyenne');
@@ -8,13 +9,32 @@ if (note >= 12) {
     console.log('Tu n\' a pas la moyenne');
 }
 
-// exos 2
+Fin exos 1 */
 
-// prompt un nombre 1 = lundi
+// Option + MAJ + A pour commenter --> /* */
 
-let nombre = parseInt(prompt('Entrez un nombre entre 1 et 7'));
+/* // exos 2
 
-switch (nombre) {
+// let nb_1 = parseInt(prompt('Entrez un premier nombre')),
+//     nb_2 = parseInt(prompt('Entrez un second nombre'));
+let nb_1 = 10,
+    nb_2 = 100;
+
+    if(nb_1 < nb_2){
+        console.log('Ce nombre ' + nb_2 + ' est plus grand que ' + nb_1)
+    }else if (nb_1 > nb_2){
+        console.log('Ce nombre ' + nb_1 + ' est plus grand que ' + nb_2)
+    }else{
+        console.log('les nombres ' + nb_2 + ' et ' + nb_1 + ' sont identiques')
+    }
+// fin exos 2 */
+
+/* // exos 3
+
+// let nb_user = parseInt(prompt('Entrez un nombre entre 1 et 7'));
+let nb_user = 5; // jour de la semaine compris entre 1 et 7
+
+switch (nb_user) {
     case 1:
         console.log('Lundi');
         break;
@@ -37,7 +57,58 @@ switch (nombre) {
         console.log('Dimanche');
         break;
     default:
-        console.log('Désolé ce n\' est pas un jour de la semaine');
+        break;
 }
 
-// fin exos 2
+// fin exos 3 */
+
+/* // exos 4
+
+// let nb_user = parseInt(prompt('Entrez un nombre qui correspond au mois'));
+let nb_user = 6; // Juin
+
+switch (nb_user) {
+    case 1:
+        console.log('Janvier');
+        break;
+    case 2:
+        console.log('Fevrier');
+        break;
+    case 3:
+        console.log('Mars');
+        break;
+    case 4:
+        console.log('Avril');
+        break;
+    case 5:
+        console.log('Mai');
+        break;
+    case 6:
+        console.log('Juin');
+        break;
+    case 7:
+        console.log('Juillet');
+        break;
+    case 8:
+        console.log('Août');
+        break;
+    case 9:
+        console.log('Septembre');
+        break;
+    case 10:
+        console.log('Octobre');
+        break;
+    case 11:
+        console.log('Novembre');
+        break;
+    case 12:
+        console.log('Décembre');
+        break;
+
+    default:
+        break;
+}
+
+// fin exos 4 */
+
+// exos 5
