70a3a613303dbf0042232ff7c96cd4fc9eb54f4b
davydebut <spanudavy@gmail.com>
2022-02-28 14:34:19 +0100

    Jeux de nim 1er essai

--- exercices/Jeux_de_Nim.js
+++ exercices/Jeux_de_Nim.js
@@ -88,9 +88,41 @@ console.log(joueur1.reste()); */
 let nomJoueur1 = 'Julien',
     nomJoueur2 = 'Thomas',
     allumettes = 16,
-    nb_saisi = 0,
-    nbrTours = 0;
+    nb_saisi,
+    nbrToursJoueur1 = 0,
+    nbrToursJoueur2 = 0;
 
 while (allumettes != 0) {
-    console.log(nomJoueur1+' combien d\'allumettes tu veux prendre ? entre 1 et 3 :');
+
+    console.log(nomJoueur1 + ' combien d\'allumettes tu veux prendre entre 1 & 3');
+    nb_saisi = 2;
+
+    if (nb_saisi >= 1 && nb_saisi <= 3) {
+        allumettes = allumettes - nb_saisi;
+        console.log(allumettes);
+        nbrToursJoueur1++;
+    }
+
+    console.log(nomJoueur2 + ' combien d\'allumettes tu veux prendre entre 1 & 3');
+    nb_saisi = 2;
+
+    if (nb_saisi >= 1 && nb_saisi <= 3) {
+        allumettes = allumettes - nb_saisi;
+        console.log(allumettes);
+        nbrToursJoueur2++;
+    }
+
+    console.log(nbrToursJoueur1);
+    console.log(nbrToursJoueur2);
+    console.log(allumettes);
+}
+
+console.log(nbrToursJoueur1);
+console.log(nbrToursJoueur2);
+console.log(allumettes);
+
+if (nbrToursJoueur1 > nbrToursJoueur2) {
+    console.log(nomJoueur1 + ' Tu à gagné !');
+} else {
+    console.log(nomJoueur2 + ' Tu à gagné !');
 }
