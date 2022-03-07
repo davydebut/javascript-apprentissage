1b514693f4cc47351fd3af41666484a4a9c68485
lafeehalima <hcm2@hotmail.fr>
2022-03-07 16:46:22 +0100

    Nodelist

--- exercices/01-EXOS-DOM-VIERGE(1)/01-EXOS-DOM-VIERGE/exo2_vierge/exo2.js
+++ exercices/01-EXOS-DOM-VIERGE(1)/01-EXOS-DOM-VIERGE/exo2_vierge/exo2.js
@@ -1,8 +1,11 @@
 // Ecrire la fonction compterElements
 function compterElements(selecteur) {
     // votre code ...
+    let nbrElmt = document.querySelectorAll(selecteur);
+    return nbrElmt.length;
 }
 
+// console.log(document.body.childNodes);
 console.log(compterElements("p")); // Doit afficher 4
 console.log(compterElements(".adjectif")); // Doit afficher 3
 console.log(compterElements("p .adjectif")); // Doit afficher 3
--- exercices/01-EXOS-DOM-VIERGE(1)/01-EXOS-DOM-VIERGE/exo3_vierge/exo3.js
+++ exercices/01-EXOS-DOM-VIERGE(1)/01-EXOS-DOM-VIERGE/exo3_vierge/exo3.js
@@ -1,9 +1,24 @@
+let id = document.querySelectorAll("[id]");
+let classAttr = document.querySelectorAll("[id]");
+console.log(id, id.length);
+// console.log(id[0].attributes[0].textContent);
+
 function infosLiens() {
     // votre code ...
+    let liens = document.querySelectorAll("a");
+    console.log(liens.length);
+    console.log(liens[0], liens[liens.length - 1]);
 }
 
-function possede(_id, _class) {  
+for (let key of id.values(id)) {
+    console.log(key);
+    let nameId = key.attributes[0];
+    console.log(nameId);
+}
+
+function possede(_id, _class) {
     // votre code ...
+    
 }
 
 infosLiens();
