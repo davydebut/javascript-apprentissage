2044b48af745f28f2a1ee0da633a5e14a6c5c398
davydebut <spanudavy@gmail.com>
2022-03-14 12:55:02 +0100

    Verifiaction exercice procédural question 6

--- Gestion_d'assurance/app.js
+++ Gestion_d'assurance/app.js
@@ -24,6 +24,32 @@ let bouton = document.querySelector('button');
 
 // let age, permis, fidelite, accident; (Debogage)
 
+/* if ((accident == 0) && (age < 25 && permis < 2)) {
+        // console.log('Assuré au tarifs rouge');
+        message.innerHTML = 'div class="alert alert-danger" role="alert">Assuré au tarif rouge</div>';
+    } else if ((accident == 0) && (age < 25 && permis > 2) || (age > 25 && permis < 2)) {
+        // console.log('Assuré au tarifs orange');
+        message.innerHTML = 'div class="alert alert-warning" role="alert">Assuré au tarif orange</div>';
+    } else if (accident == 1) {
+        // console.log('Assuré au tarifs rouge');
+        message.innerHTML = 'div class="alert alert-danger" role="alert">Assuré au tarif rouge</div>';
+    } else if ((accident == 0) && (age > 25 && permis > 2)) {
+        // console.log('Assuré au tarifs vert');
+        message.innerHTML = 'div class="alert alert-success" role="alert">Assuré au tarif vert</div>';
+    } else if (accident == 1) {
+        // console.log('Assuré au tarifs orange');
+        message.innerHTML = 'div class="alert alert-warning" role="alert">Assuré au tarif orange</div>';
+    } else if (accident == 2) {
+        // console.log('Assuré au tarifs rouge');
+        message.innerHTML = 'div class="alert alert-danger" role="alert">Assuré au tarif rouge</div>';
+    } else if (fidelite > 1 && accident == 0) {
+        // console.log('Assuré au tarifs bleu');
+        message.innerHTML = 'div class="alert alert-primary" role="alert">Assuré au tarif bleu</div>';
+    } else {
+        // console.log('Refus');
+        message.innerHTML = '<div class="alert alert-dark" role="alert">Refusé</div>';
+    } */
+
 function situation() {
 
     let message = document.querySelector('#message'),
@@ -32,32 +58,6 @@ function situation() {
         permis = document.querySelector('#permis').value,
         age = document.querySelector('#age').value,
         compteur = 0;
-
-    // if ((accident == 0) && (age < 25 && permis < 2)) {
-    //     // console.log('Assuré au tarifs rouge');
-    //     message.innerHTML = 'div class="alert alert-danger" role="alert">Assuré au tarif rouge</div>';
-    // } else if ((accident == 0) && (age < 25 && permis > 2) || (age > 25 && permis < 2)) {
-    //     // console.log('Assuré au tarifs orange');
-    //     message.innerHTML = 'div class="alert alert-warning" role="alert">Assuré au tarif orange</div>';
-    // } else if (accident == 1) {
-    //     // console.log('Assuré au tarifs rouge');
-    //     message.innerHTML = 'div class="alert alert-danger" role="alert">Assuré au tarif rouge</div>';
-    // } else if ((accident == 0) && (age > 25 && permis > 2)) {
-    //     // console.log('Assuré au tarifs vert');
-    //     message.innerHTML = 'div class="alert alert-success" role="alert">Assuré au tarif vert</div>';
-    // } else if (accident == 1) {
-    //     // console.log('Assuré au tarifs orange');
-    //     message.innerHTML = 'div class="alert alert-warning" role="alert">Assuré au tarif orange</div>';
-    // } else if (accident == 2) {
-    //     // console.log('Assuré au tarifs rouge');
-    //     message.innerHTML = 'div class="alert alert-danger" role="alert">Assuré au tarif rouge</div>';
-    // } else if (fidelite > 1 && accident == 0) {
-    //     // console.log('Assuré au tarifs bleu');
-    //     message.innerHTML = 'div class="alert alert-primary" role="alert">Assuré au tarif bleu</div>';
-    // } else {
-    //     // console.log('Refus');
-    //     message.innerHTML = '<div class="alert alert-dark" role="alert">Refusé</div>';
-    // }
 
     if (accident >= 3) {
         message.innerHTML = '<div class="alert alert-dark" role="alert">Refusé</div>';
@@ -95,12 +95,6 @@ function situation() {
     }
 }
 
-// }
-
-// }
-
 bouton.addEventListener('click', situation);
 
 // console.log(situation(26, 6, 2, 0)); (Debogage)
-
-// 925€ / 2 ce mois ci + 903€
