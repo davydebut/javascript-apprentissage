906c5cefcae09661c11578320797c66cf7293779
lafeehalima <hcm2@hotmail.fr>
2022-03-16 16:45:53 +0100

    Fin JavascriptJeanPhillipe/Exercices_JS_2.pdf

--- JavascriptJeanPhillipe/Exercices_JS_2/Exercices_JS_2.js
+++ JavascriptJeanPhillipe/Exercices_JS_2/Exercices_JS_2.js
@@ -21,7 +21,7 @@ for (let i = 0; i <= max; i++) {
 }
 
 
-// Exercice 7 : Les tableaux */
+// Exercice 7 : Les tableaux 
 
 let tableau = [];
 
@@ -40,3 +40,32 @@ for (let j = 0; j < tableau.length; j++) {
 }
 
 // Exercice 8 : Les fonctions
+
+function calcul() {
+    let sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        sum += arguments[i];
+    }
+    console.log(sum);
+}
+
+calcul(34, 2, 65, 99);
+calcul(35, 1, 66, 88);
+
+// Exercice 9 : Les objets littéraux */
+
+let famille = {
+    nom: "Smith",
+    père: "John",
+    maman: "Martine",
+    fils: "Jean",
+    fille: "Melanie"
+};
+
+console.log(famille);
+
+for (const key in famille) {
+    if (famille[key] != famille.nom) {
+        console.log(famille[key] + ' ' + famille.nom);
+    }
+}
